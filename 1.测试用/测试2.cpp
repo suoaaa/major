acf42f5fdb46f4cde83a11161f73fa2da253e429
suoaaa <1062173732@qq.com>
2024-10-16 17:48:09 +0800

    路径更正-24.10.16

--- 1.测试用/测试2.cpp
+++ 1.测试用/测试2.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
-using namespace std;
-int main(){
-    
+#include <stdio.h>
+
+int main(void){
+    int n,d1,d2,d3,d4;
+    n=12;
+    scanf("%d",&n);
+    d4=n%2; n/=2;
+    d3=n%2; n/=2;
+    d2=n%2; n/=2;
+    d1=n%2; n/=2;
+    printf("%d%d%d%d",d1,d2,d3,d4);
+    return 0;
 }
